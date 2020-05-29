927f25526b69326f4da378ace7313f816d8c7ca5
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-05-29 01:01:12 -0300

    Creates collection through code

---
+++ create-category.js
@@ -0,0 +1,20 @@
+
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./firebase.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://acess-database.firebaseio.com'
+});
+
+const dbFirebase = admin.firestore();
+
+const createDoc = dbFirebase.collection('categories').doc();
+createDoc
+    .set({
+        category: 'Category creating that code'
+    })
+    .then( snap => {
+        console.log(snap)
+    })
