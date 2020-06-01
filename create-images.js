987856618774b078a7ee67d840ee7eeebac0c6ff
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-01 18:21:02 -0300

    Creates file for add images on database

---
+++ create-images.js
@@ -0,0 +1,24 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./firebase.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://acess-database.firebaseio.com'
+});
+
+const dbFirebase = admin.firestore();
+const DOC_PRODUCT_ID = 'icWUpPrOmOftarZUoa33';
+const imageRefence = dbFirebase
+    .collection('products')
+    .doc(DOC_PRODUCT_ID)
+    .collection('images')
+    .doc();
+
+imageRefence
+    .set({
+        description: 'My first description image',
+        url: 'url image'
+    })
+    .then( res => console.log(res))
+    .catch( err => console.log(err));
