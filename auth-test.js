a817a15fdefee98de7ad6fb1b00f47734ee8d74e
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-07 02:16:48 -0300

    Moves files for directory poc

--- auth-test.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-
-const serviceAccount = require('./firebase.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://acess-database.firebaseio.com'
-});
