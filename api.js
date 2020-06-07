92056f1ba1a5b34c8f06d820fe85e50c94cdd8d7
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-07 03:19:49 -0300

    Add function insert category to available application

--- api.js
+++ api.js
@@ -3,3 +3,13 @@ const categories = require('./categories');
 categories.findAll().then(res => {
     console.log(res)
 })
+
+const insertCategory = async() => {
+
+    await categories.createCategory({
+        category: 'New oeder category'
+    })
+
+    const cat = await categories.findAll();
+    console.log(cat)
+}
