80f1f3ca949d86b0879d019bff7d5b770945ea12
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-14 23:54:59 -0300

    Add reference category on create

--- products.js
+++ products.js
@@ -3,8 +3,10 @@ const db = require('./services/firestore');
 const create = async ({ categories, ...data }) => {
 
     const doc = db.collection('products').doc();
+    const categoriesRefs = categories.map(cat => db.collection('categories').doc(cat));
     await doc.set({
         ...data,
+        categories: categoriesRefs,
         categories2: categories
     });
 }
