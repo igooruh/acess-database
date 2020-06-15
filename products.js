ee5ffa5b2499a0d3d7a1f026b14d1b90fd0e29fd
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-15 00:16:59 -0300

    Creates function to update data

--- products.js
+++ products.js
@@ -1,4 +1,5 @@
 const db = require('./services/firestore');
+const admin = require('firebase-admin');
 
 const create = async ({ categories, ...data }) => {
 
@@ -11,10 +12,15 @@ const create = async ({ categories, ...data }) => {
     });
 }
 
-const update = async(id, data) => {
+const update = async(id, { categories, ...data }) => {
 
+    const categoriesRefs = categories.map(cat => db.collection('categories').doc(cat));
     const doc = db.collection('products').doc(id);
-    await doc.update(data);
+    await doc.update({
+        ...data,
+        categories: admin.firestore.FieldValue.arrayUnion(...categoriesRefs),
+        categories2: admin.firestore.FieldValue.arrayUnion(...categories)
+    });
 }
 
 const remove = async id => {
